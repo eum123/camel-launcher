63f2c15a529ddd1d60061444272cd6f4b733efa6
manjineum <manjineum@wemakeprice.com>
2018-11-12 18:36:21 +0900

    drag & drop 변경

--- src/main/webapp/resources/js/route/route.js
+++ src/main/webapp/resources/js/route/route.js
@@ -34,204 +34,108 @@ function Route(fileName) {
             var routeId = value.id;
             var list = value.list;
 
-            var routeDivElement = $('<div id="routeDiv_' + routeId + '">');
+            var panel = $('<div />', {
+                class: 'card'
+            });
+
+            var panelBody = $('<div/>', {
+               class: 'card-body content'
+            });
+            var routeArea = $('<div/>', {
+                id: routeId
+            })
 
             list.forEach(function (value, index, array){
+                if(value.tagName == "description") {
+                    var cardHeader = $('<div />', {
+                        class: 'card-header'
+                    });
 
-                var styleString = "";
-                if(index == 0 || index == (array.length -1 )) {
-                    styleString = 'style="opacity: 1;"'
-                }
-                var divElement = $('<div class="column" draggable="true" ' + styleString + ' />', {
-                    class: 'column',
-                     draggable: true,
-                    'dragstart': function(e) {
-                        console.log(e);
-                        //this.handleDragStart(e);
-                    },
-                    dragenter: function(e) {
-                        this.handleDragEnter(e);
-                    },
-                    dragover: function(e) {
-                        this.handleDragOver(e);
-                    },
-                    dragleave: function(e) {
-                        this.handleDragLeave(e);
-                    },
-                    drop: function(e) {
-                        this.handleDrop(e);
-                    },
-                    'dragend': function(e) {
-                        console.log(e);
-                        //this.handleDragEnd(e);
-                    }
-                });
+                    addRouteTitle(routeId, cardHeader, value.description);
+                    cardHeader.appendTo(panel);
 
-
-                var headerElement = $('<header/>', {
-                    text: value.tagName
-                });
-
-                headerElement.appendTo(divElement);
-
-                divElement.appendTo(routeDivElement);
-
-                divElement.get(0).addEventListener('dragstart', this.handleDragStart);
-            });
-
-            var addElement = $('<div />', {
-                class: 'add',
-                click: function(e) {
-                    this.addNewComponent(e);
+                } else {
+                    createNode(value.tagName, value).appendTo(routeArea);
                 }
             });
 
-            // var addHeaderElement = $('<header>Unkonwn</header>');
-            // addHeaderElement.appendTo(addElement);
-            addElement.appendTo(routeDivElement);
+            routeArea.appendTo(panelBody);
+            panelBody.appendTo(panel);
 
-            routeDivElement.appendTo($('#' + divName));
+            //nodes
+            panel.appendTo($('#' + divName));
+
+            $("#" + routeId).sortable({
+                revert: true
+            });
+
         });
 
-
-        //this.initEvent();
     }
 
-    // Using this polyfill for safety.
-    Element.prototype.hasClassName = function (name) {
-        return new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)").test(this.className);
-    };
+    function addRouteTitle(routeId, cardHeader, description) {
+        var div = $('<div/>', {
+            class: "row"
+        })
 
-    Element.prototype.addClassName = function (name) {
-        if (!this.hasClassName(name)) {
-            this.className = this.className ? [this.className, name].join(' ') : name;
-        }
-    };
+        //설명
+        var descriptionDiv = $('<div/>', {
+            class: "col-sm-10",
+            text: description
+        });
+        descriptionDiv.css('text-align', 'left');
 
-    Element.prototype.removeClassName = function (name) {
-        if (this.hasClassName(name)) {
-            var c = this.className;
-            this.className = c.replace(new RegExp("(?:^|\\s+)" + name + "(?:\\s+|$)", "g"), "");
-        }
-    };
-
-    // this.initEvent = function() {
-    //
-    //     var id_ = 'routeDiv_process';
-    //     var cols_ = document.querySelectorAll('#' + id_ + ' .column');
-    //
-    //     [].forEach.call(cols_, function (col) {
-    //         col.setAttribute('draggable', 'true');  // Enable columns to be draggable.
-    //         col.addEventListener('dragstart', this.handleDragStart, false);
-    //         col.addEventListener('dragenter', this.handleDragEnter, false);
-    //         col.addEventListener('dragover', this.handleDragOver, false);
-    //         col.addEventListener('dragleave', this.handleDragLeave, false);
-    //         col.addEventListener('drop', this.handleDrop, false);
-    //         col.addEventListener('dragend', this.handleDragEnd, false);
-    //
-    //     });
-    //
-    //     var add_ = document.querySelectorAll('div .add');
-    //     [].forEach.call(add_, function (col) {
-    //         col.addEventListener('click', this.handleClick, false);
-    //     });
-    // }
-
-    var dragSrcEl_ = null;
-
-    Element.prototype.handleDragStart = function (e) {
-        console.log(e);
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/html', this.innerHTML);
-
-        dragSrcEl_ = this;
-
-        // this/e.target is the source node.
-        this.addClassName('moving');
-    };
-
-    Element.prototype.handleDragOver = function (e) {
-        if (e.preventDefault) {
-            e.preventDefault(); // Allows us to drop.
-        }
-
-        e.dataTransfer.dropEffect = 'move';
-
-        return false;
-    };
-
-    Element.prototype.handleDragEnter = function (e) {
-        this.addClassName('over');
-    };
-
-    Element.prototype.handleDragLeave = function (e) {
-        // this/e.target is previous target element.
-        this.removeClassName('over');
-    };
-
-    Element.prototype.handleDrop = function (e) {
-        // this/e.target is current target element.
-
-        if (e.stopPropagation) {
-            e.stopPropagation(); // stops the browser from redirecting.
-        }
-
-        // Don't do anything if we're dropping on the same column we're dragging.
-        if (dragSrcEl_ != this) {
-            // 반복
-            dragSrcEl_.innerHTML = this.innerHTML;
-            this.innerHTML = e.dataTransfer.getData('text/html');
-
-            // Set number of times the column has been moved.
-            // var count = this.querySelector('.count');
-            // var newCount = parseInt(count.getAttribute('data-col-moves')) + 1;
-            // count.setAttribute('data-col-moves', newCount);
-            // count.textContent = 'moves: ' + newCount;
+        //추가 button
+        var buttonDiv = $('<div/>', {
+            class: "col-sm-2"
+        });
+        buttonDiv.css('text-align', 'right');
+        var button = $('<button/>', {
+            type: "button",
+            class: "btn btn-outline-primary btn-sm",
+            text: "ADD",
+            click: function(e) {
+                addNewNode(routeId, e);
+            }
+        })
+        button.appendTo(buttonDiv);
 
 
-        }
+        descriptionDiv.appendTo(div);
+        buttonDiv.appendTo(div);
 
-        return false;
-    };
+        div.appendTo(cardHeader);
+    }
 
-    Element.prototype.handleDragEnd = function (e) {
 
-        // this/e.target is the source node.
-        [].forEach.call(cols_, function (col) {
-            col.removeClassName('over');
-            col.removeClassName('moving');
+    function createNode(name, value) {
+        var div = $('<div />', {
+            class: 'column',
+            click: function(e) {displayConfig(name, value, e)}
         });
 
-    };
+        var header = $('<header/>', {
+            text: name
+        });
+        header.appendTo(div);
 
+        return div;
+    }
 
     /**
-     * 새로운 component 추가
+     * 새로운 node 추가
      * @param e
      */
-    Element.prototype.addNewComponent = function (e) {
-        console.log(e);
+    function addNewNode (ids_, e) {
 
-        //style="opacity: 1;
-        // append unkown div
-        // var divElement = $('<div class="column" draggable="true" />');
-        //
-        // var headerElement = $('<header>Unkown</header>');
-        // headerElement.appendTo(divElement);
-        //
-        // divElement.appendTo(this.parentNode);
-        //
-        // //append 'add' div
-        // var addElement = $('<div class="add"  />');
-        // addElement.appendTo(this.parentNode);
-        //
-        // //remove add
-        // this.parentNode.removeChild(this);
-        //
-        // //event refresh
-        // this.initEvent();
+        createNode("Unkown").appendTo($('#' + ids_));
 
     };
+
+    function displayConfig(name, value, e) {
+        console.log(name);
+        console.log(value);
+    }
 
 
 }
